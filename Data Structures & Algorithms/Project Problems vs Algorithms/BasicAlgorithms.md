99e67d882d4ec81bab3d9b2d12320d4c0dc50896
lorenzowind <lorenzomart01@gmail.com>
2020-05-12 17:43:55 -0400

    Binary searches added

---
+++ Data Structures & Algorithms/Project Problems vs Algorithms/BasicAlgorithms.md
@@ -0,0 +1,10 @@
+## Linear Search - O(n)
+
+## Binary Search - O(log(n))
+
+### n * (1/2)^s = 1 -> log(n) = s
+- s is the number of steps and n is the array size
+
+## [Comparison of computational complexity](https://commons.wikimedia.org/wiki/File:Comparison_computational_complexity.svg)
+
+### n! > 2^n > n^2 > n * log(n) > n > sqrt(2) > log(n) > 1
---
+++ Data Structures & Algorithms/Project Problems vs Algorithms/binary_search_variation.py
@@ -0,0 +1,42 @@
+def recursive_binary_search(target, source, left=0):
+    if len(source) == 0:
+        return None
+    center = (len(source)-1) // 2
+    if source[center] == target:
+        return center + left
+    elif source[center] < target:
+        return recursive_binary_search(target, source[center+1:], left+center+1)
+    else:
+        return recursive_binary_search(target, source[:center], left)
+
+def find_first(target, source):
+    index = recursive_binary_search(target, source)
+    if not index:
+        return None
+    while source[index] == target:
+        if index == 0:
+            return 0
+        if source[index-1] == target:
+            index -= 1
+        else:
+            return index
+
+# Native implementation of binary search in the `contains` function.
+def contains(target, source):
+    if len(source) == 0:
+        return False
+    center = (len(source)-1) // 2
+    if source[center] == target:
+        return True
+    elif source[center] < target:
+        return contains(target, source[center+1:])
+    else:
+        return contains(target, source[:center])
+
+multiple = [1, 3, 5, 7, 7, 7, 8, 11, 12, 13, 14, 15]
+print(find_first(7, multiple)) # Should return 3
+print(find_first(9, multiple)) # Should return None
+
+letters = ['a', 'c', 'd', 'f', 'g']
+print(contains('c', letters)) ## True
+print(contains('b', letters)) ## False

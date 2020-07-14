9e3580c985a3dce5c5c3cdd0cc8151de20cb4150
lorenzowind <lorenzomart01@gmail.com>
2020-07-14 16:53:09 -0400

    Django sample application with swagger and sphinx added

--- Django Sample Application/products/models.py
+++ Django Sample Application/products/models.py
@@ -1,27 +1,60 @@
+"""
+    Modelos do app de Produtos.
+"""
 from django.db import models
-from django.db.models import Q
+from django.db.models import Q, QuerySet
+
 
 # Create your models here.
 
 class ProductManager(models.Manager):
-    def with_text(self, text):
+    def with_text(self, text: str) -> QuerySet:
+        """
+            Realiza a pesquisa nos produtos cujo nome contenha **text**.
+
+            :param text: Texto que será usado na pesquisa
+
+            :return: QuerySet com o filtro aplicado
+        """
         queryset = self.get_queryset().filter(name__contains=text)
         return queryset
 
-    def expensive_products(self):
+    def expensive_products(self) -> QuerySet:
+        """
+            Realiza o filtro dos produtos cujo preço seja maior do que 500,00 R$.
+
+            :return: QuerySet com o filtro aplicado
+        """
+        # Executa a queryset com o filtro maior ou igual a 500
         return self.get_queryset().filter(price__gte=500)
 
-    def cheap_toys(self):
+    def cheap_toys(self) -> QuerySet:
+        """
+            Retorna a lista com os brinquedos mais baratos.
+
+            :return: QuerySet com o filtro aplicado
+        """
         return self.get_queryset().filter(
             category__name='Brinquedos',
             price__lte=100
         )
 
-    def toys_or_expensive_items(self):
+    def toys_or_expensive_items(self) -> QuerySet:
+        """
+            Retorna a lista com os brinquedos mais caros.
+
+            :return: QuerySet com o filtro aplicado
+        """
         query_filter = Q(category__name='Brinquedos') | Q(price__gte=500)
         queryset = self.get_queryset().filter(query_filter)
         print(queryset.query)
         return queryset
+
+    def test_function(self):
+        a = self.with_text("")
+        b = self.expensive_products()
+        c = self.cheap_toys()
+        d = self.toys_or_expensive_items()
 
 
 class Category(models.Model):
@@ -36,6 +69,10 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    """
+        Model contendo todos os campos necessários para cadastrar produtos no
+        ecomm.
+    """
     objects = ProductManager()
     name = models.CharField('Nome', max_length=100)
     description = models.TextField('Descrição')
